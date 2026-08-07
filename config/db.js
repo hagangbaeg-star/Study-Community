f58f9485f0a31f07cf939901eaac72bf2eb083f0
백강하 <hagangbaeg@gmail.com>
2026-08-07 16:51:14 +0900

    Connect Aiven DB

--- config/db.js
+++ config/db.js
@@ -1,10 +1,14 @@
 const mysql = require("mysql2");
 
 const db = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "1101",
-    database: "studycommunity"
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
+    ssl: {
+        rejectUnauthorized: false
+    }
 });
 
 db.connect((err) => {
