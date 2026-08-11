ad89023985592aad370fd32b7d09935ea9e6c0f3
hagangbaeg-star <hagangbaeg@gmail.com>
2026-08-11 19:41:05 +0900

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,23 +1,30 @@
 const mysql = require("mysql2");
 
-const db = mysql.createConnection({
+const db = mysql.createPool({
     host: process.env.DB_HOST,
     port: Number(process.env.DB_PORT),
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB_NAME,
+
     ssl: {
         rejectUnauthorized: false
-    }
+    },
+
+    waitForConnections: true,
+    connectionLimit: 10,
+    queueLimit: 0
 });
 
-db.connect((err) => {
+db.getConnection((err, connection) => {
     if (err) {
         console.log("DB 연결 실패");
         console.log(err);
-    } else {
-        console.log("DB 연결 성공");
+        return;
     }
+
+    console.log("DB 연결 성공");
+    connection.release();
 });
 
 module.exports = db;
